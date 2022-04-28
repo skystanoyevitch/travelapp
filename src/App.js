2e1299a57b5675ba83243e06457da604f7141b32
Sky <RolyPolie@users.noreply.github.com>
2022-04-28 13:07:09 -0700

    initial full app creation commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+// import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+import { CssBaseline, Grid } from "@material-ui/core";
+import { getPlacesData } from "./api";
+import { getWeatherData } from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [places, setPlaces] = useState([]);
+	const [filteredPlaces, setFilteredPlaces] = useState([]);
+	const [coordinates, setCoordinates] = useState({});
+	const [bounds, setBounds] = useState({});
+	const [childClicked, setChildClicked] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const [type, setType] = useState("restaurants");
+	const [rating, setRating] = useState("");
+
+	const [weatherData, setWeatherData] = useState([]);
+
+	useEffect(() => {
+		const filtered = places.filter(
+			(place) => Number(place.rating) > rating
+		);
+		setFilteredPlaces(filtered);
+	}, [rating]);
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(
+			({ coords: { latitude, longitude } }) => {
+				setCoordinates({ lat: latitude, lng: longitude });
+			}
+		);
+	}, []);
+
+	useEffect(() => {
+		if (bounds.sw && bounds.ne) {
+			setIsLoading(true);
+
+			getWeatherData(coordinates.lat, coordinates.lng).then((data) =>
+				setWeatherData(data)
+			);
+			getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+				console.log(data);
+				setPlaces(
+					data?.filter((place) => place.name && place.num_reviews > 0)
+				);
+				setFilteredPlaces([]);
+				setRating("");
+				setIsLoading(false);
+			});
+		}
+	}, [bounds, type]);
+
+	return (
+		<>
+			<CssBaseline />
+			<Header setCoordinates={setCoordinates} />
+			<Grid container spacing={3} style={{ width: "100%" }}>
+				<Grid item xs={12} md={4}>
+					<List
+						places={filteredPlaces.length ? filteredPlaces : places}
+						childClicked={childClicked}
+						isLoading={isLoading}
+						type={type}
+						setType={setType}
+						rating={rating}
+						setRating={setRating}
+					/>
+				</Grid>
+				<Grid item xs={12} md={8}>
+					<Map
+						setCoordinates={setCoordinates}
+						setBounds={setBounds}
+						coordinates={coordinates}
+						places={filteredPlaces.length ? filteredPlaces : places}
+						setChildClicked={setChildClicked}
+						weatherData={weatherData}
+					/>
+				</Grid>
+			</Grid>
+		</>
+	);
+};
 
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+	try {
+		const {
+			data: { data },
+		} = await axios.get(
+			`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+			{
+				params: {
+					bl_latitude: sw.lat,
+					bl_longitude: sw.lng,
+					tr_longitude: ne.lng,
+					tr_latitude: ne.lat,
+				},
+				headers: {
+					"X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+					"X-RapidAPI-Key":
+						process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+				},
+			}
+		);
+
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const getWeatherData = async (lat, lng) => {
+	try {
+		const { data } = await axios.get(
+			"https://community-open-weather-map.p.rapidapi.com/weather",
+			{
+				params: {
+					lat: lat,
+					lon: lng,
+				},
+				headers: {
+					"X-RapidAPI-Host":
+						"community-open-weather-map.p.rapidapi.com",
+					"X-RapidAPI-Key":
+						process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+				},
+			}
+		);
+
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
